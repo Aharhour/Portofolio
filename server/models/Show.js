8400622e3b205fd50d1c4d71ecc6183358aef15c
Adil Harhour <adilharhour@gmail.com>
2026-04-01 00:39:36 +0200

    all

---
+++ server/models/Show.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const showSchema = new mongoose.Schema(
+    {
+        movie_id: { type: String, required: true, ref: "Movie" },
+        showDateTime: { type: Date, required: true },
+        showPrice: { type: Number, required: true },
+        occupiedSeats: {type: Object, default: {} },
+    }, { minimize: false }
+ )
+
+const Show = mongoose.model("Show", showSchema);
+
+export default Show;
